b059bf7e97cadbad1f14dea8c86e4b006a5a7669
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-04-12 18:35:26 -0400

    New version 0.1.12

--- tools/release.sh
+++ tools/release.sh
@@ -2,9 +2,8 @@
 set -e
 cargo bump patch
 cargo check
-cargo build --release
 cargo test
-./tools/gh-md-toc --insert --no-backup --hide-footer README.
+./tools/gh-md-toc --insert --no-backup --hide-footer README.md
 new_version=$(grep ^version Cargo.toml | awk '{print $3}' | sed s/\"//g)
 git commit -am "New version ${new_version}"
 echo "Releasing version v${new_version}"
