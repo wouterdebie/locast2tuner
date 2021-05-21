ceedbd4ffe68dac3d560826f8835549aeb1fc2eb
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-05-21 11:39:44 -0500

    Some more info in the release script

--- tools/release.sh
+++ tools/release.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
+echo "Releasing.."
+echo "=== cargo check ==="
 cargo check
+echo "=== cargo clippy ==="
 cargo clippy -- -D warnings
+echo "=== cargo test ==="
 cargo test
+echo "=== cargo bump ==="
 cargo bump patch
+echo "=== cargo check ==="
 cargo check
+echo "=== Updating TOC ==="
 ./tools/gh-md-toc --insert --no-backup --hide-footer README.md
 new_version=$(grep ^version Cargo.toml | awk '{print $3}' | sed s/\"//g)
+echo "=== git commit ==="
 git commit -am "New version ${new_version}"
-echo "Releasing version v${new_version}"
+echo "=== Tagging version v${new_version} ==="
 git tag v${new_version}
+echo "=== git push ==="
 git push && git push --tags
