0caef49f0b28b7f4e4a527793867f683b2020ce8
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-08-17 17:52:15 -0500

    Fix release script

--- tools/release.sh
+++ tools/release.sh
@@ -12,6 +12,7 @@ cargo bump patch
 echo "=== cargo check ==="
 cargo check
 echo "=== git commit ==="
+new_version=$(cat Cargo.toml | grep -E '^version' | cut -d'"' -f2)
 git commit -am "Release ${new_version}"
 echo "=== Tagging version v${new_version} ==="
 git tag v${new_version}
