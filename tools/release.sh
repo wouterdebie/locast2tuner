ce94c292596c847e96274c5e1d1316e74f4ad51f
Wouter de Bie <wouter.de.bie@datadoghq.com>
2021-08-21 18:13:18 -0500

    Release 0.3.0

--- tools/release.sh
+++ tools/release.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+level=${1:-patch}
 echo "Releasing.."
 echo "=== cargo check ==="
 cargo check
@@ -7,8 +8,8 @@ echo "=== cargo clippy ==="
 cargo clippy -- -D warnings
 echo "=== cargo test ==="
 cargo test
-echo "=== cargo bump ==="
-cargo bump patch
+echo "=== cargo bump ${level} ==="
+cargo bump ${level}
 echo "=== cargo check ==="
 cargo check
 echo "=== git commit ==="
